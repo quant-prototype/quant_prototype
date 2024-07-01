9a6ce576d54a3baa028b9af4daa1b06395229b44
Jun279 <33.beautifulboy@gmail.com>
2024-07-01 20:04:27 +0900

    Create PayConfirmProcedure.jsx

---
+++ src/pages/PayConfirmProcedure/PayConfirmProcedure.jsx
@@ -0,0 +1,10 @@
+import backButton from "../../assets/backWardBtn.png";
+import Layout from "../../shared/Layout";
+
+export default function PayConfirmProcedure() {
+  return (
+    <Layout>
+      
+    </Layout>
+  )
+}
