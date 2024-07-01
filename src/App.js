39461bd2af5fb6841b5f7a840c987edb8e568b52
Jun279 <33.beautifulboy@gmail.com>
2024-07-01 18:32:22 +0900

    전체 스타일 설정

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import styled from "styled-components";
 import { Routes, Route } from "react-router-dom";
+import { GlobalStyle } from "./style/globalStyle";
 import Home from "./pages/Home";
+
+const Wrapper = styled.div`
+  margin: 0 auto; /* 가운데 정렬 */
+  min-width: 100%; /* 전체 너비 */
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  background-color: black;
+  position: relative;
+`;
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />}/>
-      
-    </Routes>
+    <>
+      <GlobalStyle />
+      <Wrapper>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Wrapper>
+    </>
   );
 }
 
