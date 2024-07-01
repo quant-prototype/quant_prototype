a7b793b4ccdabfa22c361b6298ca32031a7b1a01
kmgnw <wathcpropropropro@gmail.com>
2024-07-01 18:21:00 +0900

    config processing

---
+++ src/shared/StandardButton.jsx
@@ -0,0 +1,12 @@
+import './css/StandardButton.css'
+
+function StandardButton({title, backgroundColor='blue', color='white', onClick, width='100%', height= '100%'}){
+    return(
+        <div style={{width: width, height: height}}>
+        <button className="button" style={{width:'100%', height:'100%', backgroundColor: backgroundColor, color: color, display: 'flex', justifyContent:'center', alignItems:'center'}} onClick={onClick} >
+            <div style={{color: color}}>{title}</div>
+        </button>
+        </div>
+    )
+}
+export default StandardButton;
