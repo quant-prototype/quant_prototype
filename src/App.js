12c8cbfff821e968fa04311c68ab3b9e49eec586
Jun279 <33.beautifulboy@gmail.com>
2024-07-02 21:58:38 +0900

    add animation

--- src/App.js
+++ src/App.js
@@ -6,17 +6,13 @@ import ConfirmMember from "./pages/PayConfirmProcedure/ConfirmMember/ConfirmMemb
 import QRPay from "./pages/QRpay/QRPay";
 import PayComplete from "./pages/PayComplete/PayComplete";
 import AddWithRadar from "./pages/SinglePay/AddWithRadar/AddWithRadar";
-import AskGroupPay from "./pages/MultiplePay/GroupMoney/AskGroupMoney"
+import AskGroupPay from "./pages/MultiplePay/GroupMoney/AskGroupMoney";
 import AddMemberByPhoneNumber from "./pages/AddMemberByPhoneNumber/AddMemberByPhoneNumber";
 import GroupQRPay from "./pages/MultiplePay/GroupQRpay/GroupQRPay";
 import SelectGroupMember from "./pages/SelectGroupMember/SelectGroupMember";
 import GroupPayComplete from "./pages/MultiplePay/GroupMoney/GroupComplete/PayComplete";
-<<<<<<< Updated upstream
 import UseGroupMoney from "./pages/MultiplePay/UseGroupMoney/UseGroupMoney";
-
-=======
 import { AnimatePresence } from "framer-motion";
->>>>>>> Stashed changes
 
 const Wrapper = styled.div`
   margin: 0 auto;
@@ -32,23 +28,6 @@ function App() {
   return (
     <>
       <GlobalStyle />
-<<<<<<< Updated upstream
-      <Wrapper>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/confirmMember" element={<ConfirmMember />} />
-          <Route path="/addMemberByPhoneNumber" element={<AddMemberByPhoneNumber />} />
-          <Route path="/pay" element={<QRPay />} />
-          <Route path="/payComplete" element={<PayComplete />} />
-          <Route path="/radar" element={<AddWithRadar/>}/>
-          <Route path="/useGroupMoney" element={<UseGroupMoney/>}/>
-          <Route path="/selectGroupMember" element={<SelectGroupMember />} />
-          <Route path="/askGroupPay" element={<AskGroupPay/>}/>
-          <Route path="/groupQr" element={<GroupQRPay/>}/>
-          <Route path="groupPayComplete" element={<GroupPayComplete/>}/>
-        </Routes>
-      </Wrapper>
-=======
       <AnimatePresence>
         <Wrapper>
           <Routes>
@@ -61,15 +40,14 @@ function App() {
             <Route path="/pay" element={<QRPay />} />
             <Route path="/payComplete" element={<PayComplete />} />
             <Route path="/radar" element={<AddWithRadar />} />
-            <Route path="/askGroupPay" element={<GroupPay />} />
+            <Route path="/useGroupMoney" element={<UseGroupMoney />} />
             <Route path="/selectGroupMember" element={<SelectGroupMember />} />
-            <Route path="/askGroupPay" element={<GroupPay />} />
+            <Route path="/askGroupPay" element={<AskGroupPay />} />
             <Route path="/groupQr" element={<GroupQRPay />} />
             <Route path="groupPayComplete" element={<GroupPayComplete />} />
           </Routes>
         </Wrapper>
       </AnimatePresence>
->>>>>>> Stashed changes
     </>
   );
 }
